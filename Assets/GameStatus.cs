335708c9577399dfd2f77cc01327734d46dbb870
axv1017 <axv1017@sru.edu>
2019-04-21 16:23:47 -0400

    Uploaded Version 0.01
    Adds soldier and battlefield

---
+++ Assets/GameStatus.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        //If the escape key is pressed
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            //Game will exit
+            Application.Quit();
+        }
+    }
+}
